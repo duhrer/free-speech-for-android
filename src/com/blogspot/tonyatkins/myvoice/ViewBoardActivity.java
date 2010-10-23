f896cb571abe0cec5b2b6acb57f7b49024f59263
duhrer <duhrer@localhost>
2010-10-24 00:55:47 +0200

    Converted options menu to XML file and added icons.  Fixed refresh of display after exiting preferences.

--- src/com/blogspot/tonyatkins/myvoice/ViewBoardActivity.java
+++ src/com/blogspot/tonyatkins/myvoice/ViewBoardActivity.java
@@ -8,6 +8,7 @@ import android.media.AudioManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.GridView;
 import android.widget.Toast;
@@ -18,9 +19,6 @@ import com.blogspot.tonyatkins.myvoice.model.ButtonListAdapter;
 import com.blogspot.tonyatkins.myvoice.model.SoundButton;
 
 public class ViewBoardActivity extends Activity {
-	public final static int ADD_ITEM = 0;
-	public static final int PREFERENCES = 1;
-	
 	SoundReferee soundReferee;
 	ButtonListAdapter buttonListAdapter;
 	
@@ -38,8 +36,7 @@ public class ViewBoardActivity extends Activity {
         // get hold of the actual grid view so we can back it with an adapter
         gridView = (GridView) findViewById(R.id.gridview);
         
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        gridView.setNumColumns(Integer.parseInt(preferences.getString("columns", "4")));
+        setColumns();
         
         // get the database connection and our content
 		dbAdapter = new DbAdapter(this);
@@ -71,22 +68,30 @@ public class ViewBoardActivity extends Activity {
         
         // FIXME:  One button should have a long-press option to actually exit the program or at least toggle "safe" mode.
     }
+
+	private void setColumns() {
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        gridView.setNumColumns(Integer.parseInt(preferences.getString("columns", "4")));
+        gridView.invalidate();
+	}
     
 	public boolean onCreateOptionsMenu(Menu menu) {
-		menu.add(0,ADD_ITEM,0,"Add Item");
-		menu.add(0,PREFERENCES,0,"Preferences");
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.view_board_menu, menu);
 		
 		return super.onCreateOptionsMenu(menu);
 	}
 
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-		case ADD_ITEM:
+		case R.id.add_button_menu_item:
 	        Intent addButtonIntent = new Intent(this, EditButtonActivity.class);
 	        startActivityForResult(addButtonIntent,EditButtonActivity.ADD_BUTTON);
-		case PREFERENCES:
+	        break;
+		case R.id.preferences_menu_item:
 			Intent preferencesIntent = new Intent(this,PreferencesActivity.class);
 			startActivityForResult(preferencesIntent,PreferencesActivity.EDIT_PREFERENCES);
+			break;
 		}
 		
 		return super.onOptionsItemSelected(item);
@@ -97,32 +102,38 @@ public class ViewBoardActivity extends Activity {
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 		
+		// Stuff that has to return data in order to be useful
 		if (data != null && resultCode == RESULT_OK) {
 			Bundle bundle = data.getExtras();
 			if (bundle != null) {
 				String returnedButtonData = bundle.getString(SoundButton.BUTTON_BUNDLE);
 				if (returnedButtonData != null && returnedButtonData.length() > 0) {
-					switch (requestCode) {
-						case EditButtonActivity.ADD_BUTTON:
-							dbAdapter.createButton(new SoundButton(returnedButtonData));
-							buttonListAdapter.refresh();
-							gridView.invalidateViews();
-							Toast.makeText(this, "Button added...", Toast.LENGTH_LONG).show();
-							break;
-						case EditButtonActivity.EDIT_BUTTON:
-							dbAdapter.updateButton(new SoundButton(returnedButtonData));
-							buttonListAdapter.refresh();
-							gridView.invalidateViews();
-							Toast.makeText(this, "Button updated...", Toast.LENGTH_LONG).show();
-							break;
-						case PreferencesActivity.EDIT_PREFERENCES:
-							Toast.makeText(this, "Preferences Saved", Toast.LENGTH_LONG);
-							break;
-					}
+				}
+				switch (requestCode) {
+					case EditButtonActivity.ADD_BUTTON:
+						dbAdapter.createButton(new SoundButton(returnedButtonData));
+						buttonListAdapter.refresh();
+						gridView.invalidateViews();
+						Toast.makeText(this, "Button added...", Toast.LENGTH_LONG).show();
+						break;
+					case EditButtonActivity.EDIT_BUTTON:
+						dbAdapter.updateButton(new SoundButton(returnedButtonData));
+						buttonListAdapter.refresh();
+						gridView.invalidateViews();
+						Toast.makeText(this, "Button updated...", Toast.LENGTH_LONG).show();
+						break;
 				}
 			}
 		}
+		// Stuff that doesn't have to return data
+		else {
+			switch (requestCode) {
+				case PreferencesActivity.EDIT_PREFERENCES:
+					soundReferee.setLocale();
+					setColumns();
+					Toast.makeText(this, "Preferences Updated", Toast.LENGTH_LONG);
+					break;
+			}
+		}
 	}
-
-	
 }
