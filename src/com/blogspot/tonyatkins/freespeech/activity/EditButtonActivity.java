1070952cf0604ca723b31cc0fcc2d4c2cc2d2e6d
Tony Atkins <3868423+duhrer@users.noreply.github.com>
2012-11-17 19:43:49 +0100

    Cleaned up button editing interface.
    Migrated all button text and hints to i18n properties.
    
    Added resizing of large images to avoid running out of memory.
    
    Added support for taking photos and picking images from the gallery.

--- src/com/blogspot/tonyatkins/freespeech/activity/EditButtonActivity.java
+++ src/com/blogspot/tonyatkins/freespeech/activity/EditButtonActivity.java
@@ -22,20 +22,22 @@
  */
 package com.blogspot.tonyatkins.freespeech.activity;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
@@ -43,34 +45,36 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.ExpandableListView;
-import android.widget.LinearLayout;
-import android.widget.SimpleExpandableListAdapter;
-import android.widget.TextView;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.blogspot.tonyatkins.freespeech.Constants;
 import com.blogspot.tonyatkins.freespeech.R;
 import com.blogspot.tonyatkins.freespeech.db.DbAdapter;
+import com.blogspot.tonyatkins.freespeech.listeners.ActivityLaunchListener;
 import com.blogspot.tonyatkins.freespeech.model.FileIconListAdapter;
 import com.blogspot.tonyatkins.freespeech.model.SoundButton;
 import com.blogspot.tonyatkins.freespeech.model.Tab;
+import com.blogspot.tonyatkins.freespeech.utils.FileUtils;
 import com.blogspot.tonyatkins.freespeech.utils.TtsCacheUtils;
+import com.blogspot.tonyatkins.freespeech.view.ColorSwatch;
 import com.blogspot.tonyatkins.freespeech.view.SoundButtonView;
 import com.google.common.io.Files;
 
 public class EditButtonActivity extends FreeSpeechActivity {
-	private static final String PARAM_LABEL = "paramLabel";
-	private static final String PARAM_CATEGORY_LABEL = "paramCategoryLabel";
 	public static final int ADD_BUTTON = 0;
 	public static final int EDIT_BUTTON = 1;
 	private static final int MICROPHONE_REQUEST = 1234;
+	private static final int CAMERA_REQUEST = 2345;
+	private static final int GALLERY_REQUEST = 3456;
+
 
 	private SoundButton tempButton;
 	private boolean isNewButton = false;
 	private DbAdapter dbAdapter;
 
 	private SoundButtonView previewButton;
+	private ColorSwatch colorPickerButton;
 
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
@@ -105,38 +109,63 @@ public class EditButtonActivity extends FreeSpeechActivity {
 
 		setContentView(R.layout.edit_button);
 
-		// find and wire up the button categories, rows, and entries
-		ExpandableListView expandableListView = (ExpandableListView) findViewById(R.id.editButtonExpandableList);
+		// Wire up the text editing button
+		Intent editIntent = new Intent(this, EditTextActivity.class);
+		editIntent.putExtra(EditTextActivity.TEXT_TYPE, SoundButton.TTS_TEXT_TYPE);
+		editIntent.putExtra(SoundButton.BUTTON_BUNDLE, tempButton.getSerializable());
+		
+		Button editTextButton = (Button) findViewById(R.id.editButtonTtsText);
+		editTextButton.setOnClickListener(new ActivityLaunchListener(this, EditTextActivity.REQUEST_CODE, editIntent));
+		
+		// Wire up the label editing button
+		Intent editLabelIntent = new Intent(this, EditTextActivity.class);
+		editLabelIntent.putExtra(EditTextActivity.TEXT_TYPE, SoundButton.LABEL_TEXT_TYPE);
+		editLabelIntent.putExtra(SoundButton.BUTTON_BUNDLE, tempButton.getSerializable());
 
-		String[] categories = { "Sound Settings", "Look and Feel" };
-		ArrayList<Map<String, String>> buttonParameterCategories = new ArrayList<Map<String, String>>();
-		for (String category : categories)
-		{
-			TreeMap<String, String> buttonParameterMap = new TreeMap<String, String>();
-			buttonParameterMap.put(PARAM_CATEGORY_LABEL, category);
-			buttonParameterCategories.add(buttonParameterMap);
-		}
+		Button editLabelButton = (Button) findViewById(R.id.editButtonLabelText);
+		editLabelButton.setOnClickListener(new ActivityLaunchListener(this, EditTextActivity.REQUEST_CODE, editLabelIntent));
+		
+		// Wire up the image file picker button
+		Intent imagePickerIntent = new Intent(this, FilePickerActivity.class);
+		imagePickerIntent.putExtra(FilePickerActivity.FILE_TYPE_BUNDLE, FileIconListAdapter.IMAGE_FILE_TYPE);
+		imagePickerIntent.putExtra(FilePickerActivity.CWD_BUNDLE, tempButton.getImagePath());
 
-		String[][] buttonsByCategory = { { "Text to Speak", "Sound File", "Record Sound" }, { "Label", "Background Color", "Image" } };
-		ArrayList parametersByCategory = new ArrayList<ArrayList<Map<String, String>>>();
-		for (String[] buttons : buttonsByCategory)
-		{
-			ArrayList<Map> buttonParameterMap = new ArrayList<Map>();
-			for (String button : buttons)
-			{
-				TreeMap<String, String> buttonLabelMap = new TreeMap<String, String>();
-				buttonLabelMap.put(PARAM_LABEL, button);
-				buttonParameterMap.add(buttonLabelMap);
-			}
-			parametersByCategory.add(buttonParameterMap);
-		}
+		Button imageFilePickerButton = (Button) findViewById(R.id.editButtonImageFileButton);
+		imageFilePickerButton.setOnClickListener(new ActivityLaunchListener(this, FilePickerActivity.REQUEST_CODE, imagePickerIntent));
+		
+		
+		// Wire up the image gallery button
+		Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+		galleryIntent.setType("image/*");
+		ImageButton galleryButton = (ImageButton) findViewById(R.id.editButtonGalleryButton);
+		galleryButton.setOnClickListener(new ActivityLaunchListener(this, GALLERY_REQUEST, galleryIntent));
+		
+		// Wire up the camera button
+		ImageButton cameraButton = (ImageButton) findViewById(R.id.editButtonCameraButton);
+		cameraButton.setOnClickListener(new ActivityLaunchListener(this,CAMERA_REQUEST,new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)));
+		
+		// Wire up the sound file picker button
+		Intent soundPickerIntent = new Intent(this, FilePickerActivity.class);
+		soundPickerIntent.putExtra(FilePickerActivity.FILE_TYPE_BUNDLE, FileIconListAdapter.SOUND_FILE_TYPE);
+		soundPickerIntent.putExtra(FilePickerActivity.CWD_BUNDLE, tempButton.getSoundPath());
 
-		SimpleExpandableListAdapter adapter = new SimpleExpandableListAdapter(this, buttonParameterCategories, R.layout.edit_button_param_category_expanded, R.layout.edit_button_param_category_closed, new String[] { PARAM_CATEGORY_LABEL }, new int[] { R.id.editButtonParamCategoryLabel }, parametersByCategory, R.layout.edit_button_param_entry, new String[] { PARAM_LABEL }, new int[] { R.id.editButtonParamLabel });
+		Button soundPickerButton = (Button) findViewById(R.id.editButtonSoundFileButton);
+		soundPickerButton.setOnClickListener(new ActivityLaunchListener(this, FilePickerActivity.REQUEST_CODE, soundPickerIntent));
+		
+		//wire up the recorder button
+		Intent recordSoundIntent = new Intent(this,RecordSoundActivity.class);
+		recordSoundIntent.putExtra(RecordSoundActivity.FILE_NAME_KEY, tempButton.getLabel() );
+		ImageButton recordSoundButton = (ImageButton) findViewById(R.id.editButtonMicrophoneButton);
+		recordSoundButton.setOnClickListener(new ActivityLaunchListener(this, RecordSoundActivity.REQUEST_CODE, recordSoundIntent));
 
-		expandableListView.setAdapter(adapter);
-		expandableListView.setOnChildClickListener(new SimpleChildClickListener(this));
-		expandableListView.expandGroup(0);
+		// Wire up the color picker and display the selected color
+		Intent colorPickerIntent = new Intent(this, ColorPickerActivity.class);
+		colorPickerIntent.putExtra(SoundButton.BUTTON_BUNDLE, tempButton.getSerializable());
 
+		colorPickerButton = (ColorSwatch) findViewById(R.id.buttonBgColorColorSwatch);
+		colorPickerButton.setOnClickListener(new ActivityLaunchListener(this, ColorPickerActivity.REQUEST_CODE, colorPickerIntent));
+		updateColorSwatch();
+		
 		// locate the preview button and hold onto its location
 		previewButton = (SoundButtonView) findViewById(R.id.editButtonPreviewButton);
 		previewButton.setSoundButton(tempButton);
@@ -149,6 +178,17 @@ public class EditButtonActivity extends FreeSpeechActivity {
 		// wire up the "save" button
 		Button saveButton = (Button) findViewById(R.id.buttonPanelSaveButton);
 		saveButton.setOnClickListener(new SaveListener(this));
+	}
+
+	private void updateColorSwatch() {
+		colorPickerButton.setBackgroundColor(Color.TRANSPARENT);
+		try {
+			if (tempButton.getBgColor() != null) {
+				colorPickerButton.setBackgroundColor(Color.parseColor(tempButton.getBgColor()));
+			}
+		} catch (IllegalArgumentException e) {
+			Toast.makeText(this, "The current color is invalid and will not be displayed.", Toast.LENGTH_LONG).show();
+		}
 	}
 
 	@Override
@@ -275,6 +315,7 @@ public class EditButtonActivity extends FreeSpeechActivity {
 					{
 						String selectedColorString = returnedBundle.getString(ColorPickerActivity.COLOR_BUNDLE);
 						setSelectedColor(selectedColorString);
+						updateColorSwatch();
 					}
 				}
 				else if (requestCode == EditTextActivity.REQUEST_CODE)
@@ -298,6 +339,23 @@ public class EditButtonActivity extends FreeSpeechActivity {
 						tempButton.setTtsText(newTtsText);
 						// There are no visible differences, so we don't need to
 						// update the display
+					}
+				}
+				else if (requestCode == CAMERA_REQUEST) {
+					Bitmap thumbnail = (Bitmap) returnedBundle.get("data");	
+					ByteArrayOutputStream output = new ByteArrayOutputStream();
+					thumbnail.compress(CompressFormat.PNG, 100, output);
+	
+					File bitmapFile = new File(Constants.IMAGE_DIRECTORY + "/" + FileUtils.generateUniqueFilename() + ".png");
+					FileOutputStream bitmapOutput;
+					try {
+						bitmapOutput = new FileOutputStream(bitmapFile);
+						bitmapOutput.write(output.toByteArray());
+						bitmapOutput.close();
+						tempButton.setImagePath(bitmapFile.getAbsolutePath());
+						Log.i(Constants.TAG, "Saved bitmap to file:" + bitmapFile.getAbsolutePath());
+					} catch (Exception e) {
+						Log.e(Constants.TAG, "Error saving picture to file:", e);
 					}
 				}
 			}
@@ -330,6 +388,43 @@ public class EditButtonActivity extends FreeSpeechActivity {
 						Toast.makeText(this, "No data returned or sound recorder failed to launch.", Toast.LENGTH_LONG).show();
 					}
 				}
+				else if (requestCode == GALLERY_REQUEST) {
+					Uri _uri = data.getData();
+					
+					if (_uri != null) {
+						Cursor cursor = getContentResolver().query(_uri, new String[] { android.provider.MediaStore.Images.ImageColumns.DATA }, null, null, null);
+						cursor.moveToFirst();
+						final String imageFilePath = cursor.getString(0);
+						cursor.close();
+
+						File bitmapFile = new File(Constants.IMAGE_DIRECTORY + "/" + FileUtils.generateUniqueFilename() + ".png");
+
+						File originalFile = new File(imageFilePath);
+						if (originalFile.exists()) {
+							try {
+								BufferedInputStream bis = new BufferedInputStream(new FileInputStream(originalFile));
+								BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(bitmapFile));
+								
+								int bytes = 0;
+								byte[] buffer = new byte[Constants.BUFFER_SIZE];
+								while ((bytes = bis.read(buffer)) != -1) {
+									bos.write(buffer);
+								}
+								bis.close();
+								bos.close();
+
+								tempButton.setImagePath(bitmapFile.getAbsolutePath());
+								Log.d(Constants.TAG, "Copied gallery file '" + imageFilePath + "' to '" + bitmapFile.getAbsolutePath() + "'.");
+							} catch (Exception e) {
+								Log.e(Constants.TAG, "Can't copy gallery file to final location.", e);
+							}
+						}
+						else {
+							Log.e(Constants.TAG, "Can't copy gallery file '" + imageFilePath + "' to our directory because it doesn't exist.");
+						}
+					}
+				}
+
 				// If no data is returned from the color picker, but the result
 				// is OK, it means the color is set to transparent (null)
 				else if (requestCode == ColorPickerActivity.REQUEST_CODE && resultCode == ColorPickerActivity.COLOR_SELECTED)
@@ -380,100 +475,4 @@ public class EditButtonActivity extends FreeSpeechActivity {
 		}
 	}
 
-	private class SimpleChildClickListener implements ExpandableListView.OnChildClickListener {
-		Intent intent;
-		private Context context;
-
-		public SimpleChildClickListener(Context context) {
-			super();
-			this.context = context;
-		}
-
-		public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
-			// TODO: Find a better way of knitting together child views and
-			// destinations
-			if (v instanceof LinearLayout && ((LinearLayout) v).getChildCount() > 0)
-			{
-				View v2 = ((LinearLayout) v).getChildAt(0);
-				if (v2 instanceof TextView)
-				{
-					String label = (String) ((TextView) v2).getText();
-
-					// This is horribly brittle, but for now this is how I'll
-					// make different decisions based on the action
-					if ("Text to Speak".equals(label))
-					{
-						// launch a text editing activity to change the text to
-						// speak
-						Intent intent = new Intent(context, EditTextActivity.class);
-						intent.putExtra(EditTextActivity.TEXT_TYPE, SoundButton.TTS_TEXT_TYPE);
-						intent.putExtra(SoundButton.BUTTON_BUNDLE, tempButton.getSerializable());
-						int requestCode = EditTextActivity.REQUEST_CODE;
-						((Activity) context).startActivityForResult(intent, requestCode);
-					}
-					else if ("Record Sound".equals(label))
-					{
-						// launch the sound recorder
-						intent = new Intent(context,RecordSoundActivity.class);
-						intent.putExtra(RecordSoundActivity.FILE_NAME_KEY, tempButton.getLabel() );
-						int requestCode = RecordSoundActivity.REQUEST_CODE;
-						((Activity) context).startActivityForResult(intent, requestCode);
-
-						// FIXME:  This doesn't see to work, at least with the HTC voice recorder
-//						final PackageManager packageManager = context.getPackageManager();
-//						final Intent intent = new Intent(android.provider.MediaStore.Audio.Media.RECORD_SOUND_ACTION);
-//						List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
-//						if (list.size() > 0)
-//						{
-//							// launch the sound recorder
-//							((Activity) context).startActivityForResult(intent, MICROPHONE_REQUEST);
-//						}
-//						else
-//						{
-//							Toast.makeText(context, "No apps available to record sounds.", Toast.LENGTH_LONG).show();
-//						}
-					}
-					else if ("Sound File".equals(label))
-					{
-						// launch the sound picker
-						Intent intent = new Intent(context, FilePickerActivity.class);
-						intent.putExtra(FilePickerActivity.FILE_TYPE_BUNDLE, FileIconListAdapter.SOUND_FILE_TYPE);
-						intent.putExtra(FilePickerActivity.CWD_BUNDLE, tempButton.getSoundPath());
-						int requestCode = FilePickerActivity.REQUEST_CODE;
-						((Activity) context).startActivityForResult(intent, requestCode);
-					}
-					else if ("Label".equals(label))
-					{
-						// launch a text editing activity to change the label
-						Intent intent = new Intent(context, EditTextActivity.class);
-						intent.putExtra(EditTextActivity.TEXT_TYPE, SoundButton.LABEL_TEXT_TYPE);
-						intent.putExtra(SoundButton.BUTTON_BUNDLE, tempButton.getSerializable());
-						int requestCode = EditTextActivity.REQUEST_CODE;
-						((Activity) context).startActivityForResult(intent, requestCode);
-					}
-					else if ("Background Color".equals(label))
-					{
-						// launch the color picker
-						intent = new Intent(context, ColorPickerActivity.class);
-						intent.putExtra(SoundButton.BUTTON_BUNDLE, tempButton.getSerializable());
-						int requestCode = ColorPickerActivity.REQUEST_CODE;
-						((Activity) context).startActivityForResult(intent, requestCode);
-					}
-					else if ("Image".equals(label))
-					{
-						// launch the image picker
-						Intent intent = new Intent(context, FilePickerActivity.class);
-						intent.putExtra(FilePickerActivity.FILE_TYPE_BUNDLE, FileIconListAdapter.IMAGE_FILE_TYPE);
-						intent.putExtra(FilePickerActivity.CWD_BUNDLE, tempButton.getImagePath());
-						int requestCode = FilePickerActivity.REQUEST_CODE;
-						((Activity) context).startActivityForResult(intent, requestCode);
-					}
-				}
-
-				return true;
-			}
-
-			return false;
-		}
-	}
 }
