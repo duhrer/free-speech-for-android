0f2d8629abbe24dcb95fbc28b0849751d116194a
Tony Atkins <duhrer@gmail.com>
2012-11-18 20:21:56 +0100

    Cleaned up startup code to recover from errors when no data exists.

--- src/com/blogspot/tonyatkins/freespeech/activity/StartupActivity.java
+++ src/com/blogspot/tonyatkins/freespeech/activity/StartupActivity.java
@@ -48,217 +48,253 @@ import com.blogspot.tonyatkins.freespeech.R;
 import com.blogspot.tonyatkins.freespeech.db.DbAdapter;
 import com.blogspot.tonyatkins.freespeech.exception.ExceptionHandler;
 import com.blogspot.tonyatkins.freespeech.listeners.ActivityQuitListener;
+import com.blogspot.tonyatkins.freespeech.model.Tab;
 
 public class StartupActivity extends FreeSpeechActivity {
-  private static final int TTS_CHECK_CODE = 777;
-  private static final int VIEW_BOARD_CODE = 241;
-  private Map<String, String> errorMessages = new HashMap<String, String>();
-  private TextToSpeech tts;
-  private ProgressDialog dialog;
-  private DbAdapter dbAdapter;
-  private Context context = this;
-  private Intent mainIntent;
+	private static final int TTS_CHECK_CODE = 777;
+	private static final int VIEW_BOARD_CODE = 241;
+	private Map<String, String> errorMessages = new HashMap<String, String>();
+	private TextToSpeech tts;
+	private ProgressDialog dialog;
+	private DbAdapter dbAdapter;
+	private Intent mainIntent;
 
-  private boolean isBoardRunning = false;
+	private boolean isBoardRunning = false;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-    setContentView(R.layout.startup);
+		setContentView(R.layout.startup);
 
-    // Wire up the exception handling
-    UncaughtExceptionHandler handler = new ExceptionHandler(this, ExceptionCatcherActivity.class);
-    Thread.setDefaultUncaughtExceptionHandler(handler);
+		// Wire up the exception handling
+		UncaughtExceptionHandler handler = new ExceptionHandler(this, ExceptionCatcherActivity.class);
+		Thread.setDefaultUncaughtExceptionHandler(handler);
 
-    // Do we have TTS and the language pack?
-    // Offer to let the user download the pack, disable TTS until we have it
-    Intent checkIntent = new Intent();
-    checkIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
-    startActivityForResult(checkIntent, TTS_CHECK_CODE);
-  }
+		// Do we have TTS and the language pack?
+		// Offer to let the user download the pack, disable TTS until we have it
+		Intent checkIntent = new Intent();
+		checkIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+		startActivityForResult(checkIntent, TTS_CHECK_CODE);
+	}
 
-  @Override
-  protected void onStart() {
-    super.onStart();
+	@Override
+	protected void onStart() {
+		super.onStart();
 
-    dialog = new ProgressDialog(this);
-    dialog.setMessage("Starting up, please stand by...");
-    dialog.setCancelable(false);
-    dialog.show();
+		dialog = new ProgressDialog(this);
+		dialog.setMessage("Starting up, please stand by...");
+		dialog.setCancelable(false);
+		dialog.show();
 
-    // Start monitoring for changes in the SD card state and quit with an
-    // error if the card is removed or damaged.
-    // We depend heavily on storage, so this must be implemented in each of
-    // our activities.
-    // FIXME: This currently doesn't work and causes problems on application
-    // finish.
-    // registerReceiver(storageUnavailableReceiver, new
-    // StorageUnavailableFilter());
+		// Start monitoring for changes in the SD card state and quit with an
+		// error if the card is removed or damaged.
+		// We depend heavily on storage, so this must be implemented in each of
+		// our activities.
+		// FIXME: This currently doesn't work and causes problems on application
+		// finish.
+		// registerReceiver(storageUnavailableReceiver, new
+		// StorageUnavailableFilter());
 
-    // Is there an sdcard to store things on?
-    if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
-      // See if we have a home directory on the SD card
-      File homeDirectory = new File(Constants.HOME_DIRECTORY);
-      if (!homeDirectory.exists()) {
-        // make our home directory if it doesn't exist
-        if (homeDirectory.mkdirs()) {
-          dialog.setMessage("Created home directory");
-        } else {
-          errorMessages.put("Can't create home directory",
-              "I wasn't able to create a home directory to store my settings.  Unable to continue.");
-        }
-      }
+		// Is there an sdcard to store things on?
+		if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState()))
+		{
+			// See if we have a home directory on the SD card
+			File homeDirectory = new File(Constants.HOME_DIRECTORY);
+			if (!homeDirectory.exists())
+			{
+				// make our home directory if it doesn't exist
+				if (homeDirectory.mkdirs())
+				{
+					dialog.setMessage("Created home directory");
+				}
+				else
+				{
+					errorMessages.put("Can't create home directory", "I wasn't able to create a home directory to store my settings.  Unable to continue.");
+				}
+			}
 
-      File soundDirectory = new File(Constants.SOUND_DIRECTORY);
-      if (!soundDirectory.exists()) {
-        if (soundDirectory.mkdir()) {
-          dialog.setMessage("Created sound directory");
-        } else {
-          errorMessages.put("Can't create sound directory",
-              "I wasn't able to create a directory to store my sounds.  Unable to continue.");
-        }
-      }
+			File soundDirectory = new File(Constants.SOUND_DIRECTORY);
+			if (!soundDirectory.exists())
+			{
+				if (soundDirectory.mkdir())
+				{
+					dialog.setMessage("Created sound directory");
+				}
+				else
+				{
+					errorMessages.put("Can't create sound directory", "I wasn't able to create a directory to store my sounds.  Unable to continue.");
+				}
+			}
 
-      File imageDirectory = new File(Constants.IMAGE_DIRECTORY);
-      if (!imageDirectory.exists()) {
-        if (imageDirectory.mkdir()) {
-          dialog.setMessage("Created image directory");
-        } else {
-          errorMessages.put("Can't create sound directory",
-              "I wasn't able to create a directory to store my sounds.  Unable to continue.");
-        }
-      }
+			File imageDirectory = new File(Constants.IMAGE_DIRECTORY);
+			if (!imageDirectory.exists())
+			{
+				if (imageDirectory.mkdir())
+				{
+					dialog.setMessage("Created image directory");
+				}
+				else
+				{
+					errorMessages.put("Can't create sound directory", "I wasn't able to create a directory to store my sounds.  Unable to continue.");
+				}
+			}
 
-      dbAdapter = new DbAdapter(this);
+			dbAdapter = new DbAdapter(this);
 
-      // Sanity check that we have data
-      Cursor buttonCursor = dbAdapter.fetchAllButtonsAsCursor();
-      Cursor tabCursor = dbAdapter.fetchAllTabs();
-      if (buttonCursor == null || tabCursor == null) {
-        errorMessages.put("Error querying database", "I wasn't able to verify the database.  Unable to continue.");
-      }
-      // It's normal to have no buttons (if the user deletes them all),
-      // but we should always have at least one tab
-      else if (tabCursor.getCount() == 0) {
-        errorMessages.put("No tab data found",
-            "I wasn't able to find any tab data in the database.  Unable to continue.");
-      }
-      buttonCursor.close();
-      tabCursor.close();
-    } else {
-      errorMessages.put("No SD card found",
-          "This application must be able to write to an SD card.  Please provide one and restart.");
-    }
+			// Sanity check that we have data
+			Cursor buttonCursor = dbAdapter.fetchAllButtonsAsCursor();
+			Cursor tabCursor = dbAdapter.fetchAllTabs();
+			int tabCount = tabCursor.getCount();
+			buttonCursor.close();
+			tabCursor.close();
+			if (buttonCursor == null || tabCursor == null)
+			{
+				errorMessages.put("Error querying database", "I wasn't able to verify the database.  Unable to continue.");
+			}
+			else if (tabCount == 0)
+			{
+				Log.e(Constants.TAG, "I wasn't able to find any tab data in the database.  Creating a tab to allow us to continue.");
+				dbAdapter.createTab(new Tab(Tab.NO_ID,"Home"));
+			}
+		}
+		else
+		{
+			errorMessages.put("No SD card found", "This application must be able to write to an SD card.  Please provide one and restart.");
+		}
 
-    TtsInitListener ttsInitListener = new TtsInitListener();
-    tts = new TextToSpeech(this, ttsInitListener);
-  }
+		TtsInitListener ttsInitListener = new TtsInitListener();
+		tts = new TextToSpeech(this, ttsInitListener);
+	}
 
-  private void launchOrDie() {
-    // if (dialog != null && dialog.isShowing()) {
-    // dialog.dismiss();
-    // }
+	private void launchOrDie() {
+		// if (dialog != null && dialog.isShowing()) {
+		// dialog.dismiss();
+		// }
 
-    if (errorMessages.size() > 0) {
-      Builder alertDialogBuilder = new AlertDialog.Builder(this);
-      Iterator<String> keyIterator = errorMessages.keySet().iterator();
-      if (errorMessages.size() == 1) {
-        String key = keyIterator.next();
-        alertDialogBuilder.setTitle(key);
-        alertDialogBuilder.setMessage(errorMessages.get(key));
-      } else {
-        alertDialogBuilder.setTitle("Multiple Errors on Startup");
+		if (errorMessages.size() > 0)
+		{
+			Builder alertDialogBuilder = new AlertDialog.Builder(this);
+			Iterator<String> keyIterator = errorMessages.keySet().iterator();
+			if (errorMessages.size() == 1)
+			{
+				String key = keyIterator.next();
+				alertDialogBuilder.setTitle(key);
+				alertDialogBuilder.setMessage(errorMessages.get(key));
+			}
+			else
+			{
+				alertDialogBuilder.setTitle("Multiple Errors on Startup");
 
-        LinearLayout errorList = new LinearLayout(this);
-        alertDialogBuilder.setView(errorList);
+				LinearLayout errorList = new LinearLayout(this);
+				alertDialogBuilder.setView(errorList);
 
-        while (keyIterator.hasNext()) {
-          String key = keyIterator.next();
-          TextView errorTextView = new TextView(this);
-          errorTextView.setText(key + ": " + errorMessages.get(key));
-          errorList.addView(errorTextView);
-        }
-      }
+				while (keyIterator.hasNext())
+				{
+					String key = keyIterator.next();
+					TextView errorTextView = new TextView(this);
+					errorTextView.setText(key + ": " + errorMessages.get(key));
+					errorList.addView(errorTextView);
+				}
+			}
 
-      AlertDialog alertDialog = alertDialogBuilder.create();
-      alertDialog.setButton("Exit", new ActivityQuitListener(this));
-      alertDialog.setOnCancelListener(new ActivityQuitListener(this));
-      alertDialog.show();
-    } else {
-      if (!isBoardRunning) {
-        isBoardRunning = true;
+			AlertDialog alertDialog = alertDialogBuilder.create();
+			alertDialog.setButton("Exit", new ActivityQuitListener(this));
+			alertDialog.setOnCancelListener(new ActivityQuitListener(this));
+			alertDialog.show();
+		}
+		else
+		{
+			if (!isBoardRunning)
+			{
+				isBoardRunning = true;
 
-        // Start the main activity
-        if (mainIntent == null) {
-          mainIntent = new Intent(this, ViewBoardActivity.class);
-        }
+				// Start the main activity
+				if (mainIntent == null)
+				{
+					mainIntent = new Intent(this, ViewBoardActivity.class);
+				}
 
-        startActivityIfNeeded(mainIntent, VIEW_BOARD_CODE);
-      }
-    }
-  }
+				startActivityIfNeeded(mainIntent, VIEW_BOARD_CODE);
+			}
+		}
+	}
 
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    if (requestCode == TTS_CHECK_CODE) {
-      if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_MISSING_DATA) {
-			Log.w(Constants.TAG, "The TTS engine indicates that you are missing data.  This may simply indicate that you have not installed unneeded languages, or it may indicate a problem.");
-      }
-      else if (resultCode != TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
-        Intent installIntent = new Intent();
-        installIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
-        startActivity(installIntent);
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == TTS_CHECK_CODE)
+		{
+			if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_MISSING_DATA)
+			{
+				Log.w(Constants.TAG, "The TTS engine indicates that you are missing data.  This may simply indicate that you have not installed unneeded languages, or it may indicate a problem.");
+			}
+			else if (resultCode != TextToSpeech.Engine.CHECK_VOICE_DATA_PASS)
+			{
+				Intent installIntent = new Intent();
+				installIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+				startActivity(installIntent);
 
-        errorMessages.put("Error Initializing TTS", "Could not verify that TTS is available.");
-      }
-    } else if (requestCode == VIEW_BOARD_CODE) {
-      if (resultCode == ViewBoardActivity.RESULT_RESTART_REQUIRED) {
-        // quick hack to restart the main activity if the preferences
-        // have changed
-        if (mainIntent != null)
-          startActivityIfNeeded(mainIntent, VIEW_BOARD_CODE);
-        else
-          finish();
-      } else {
-        // this should avoid the double restarts I've seen previously.
-        finish();
-      }
-    }
-  }
+				errorMessages.put("Error Initializing TTS", "Could not verify that TTS is available.");
+			}
+		}
+		else if (requestCode == VIEW_BOARD_CODE)
+		{
+			if (resultCode == ViewBoardActivity.RESULT_RESTART_REQUIRED)
+			{
+				// quick hack to restart the main activity if the preferences
+				// have changed
+				if (mainIntent != null)
+					startActivityIfNeeded(mainIntent, VIEW_BOARD_CODE);
+				else
+					finish();
+			}
+			else
+			{
+				// this should avoid the double restarts I've seen previously.
+				finish();
+			}
+		}
+	}
 
-  @Override
-  public void finish() {
-    if (dbAdapter != null)
-      dbAdapter.close();
-    super.finish();
-  }
+	@Override
+	public void finish() {
+		if (dbAdapter != null)
+			dbAdapter.close();
+		super.finish();
+	}
 
-  private class TtsInitListener implements OnInitListener {
-    public void onInit(int status) {
-      if (status == TextToSpeech.SUCCESS) {
-        int result = tts.setLanguage(Locale.US);
-        if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-          errorMessages.put("Error Initializing TTS", "Language is not available.");
-        }
-      } else {
-        errorMessages.put("Error Initializing TTS", "Could not initialize TextToSpeech engine.");
-      }
+	private class TtsInitListener implements OnInitListener {
+		public void onInit(int status) {
+			if (status == TextToSpeech.SUCCESS)
+			{
+				int result = tts.setLanguage(Locale.US);
+				if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED)
+				{
+					errorMessages.put("Error Initializing TTS", "Language is not available.");
+				}
+			}
+			else
+			{
+				errorMessages.put("Error Initializing TTS", "Could not initialize TextToSpeech engine.");
+			}
 
-      destroyTts();
-      launchOrDie();
-    }
+			destroyTts();
+			launchOrDie();
+		}
 
-  }
+	}
 
-  private void destroyTts() {
-    if (tts != null)
-      tts.shutdown();
-  }
+	private void destroyTts() {
+		if (tts != null)
+			tts.shutdown();
+	}
 
-  @Override
-  protected void onDestroy() {
-    destroyTts();
-    super.onDestroy();
-    if (dialog != null) { dialog.dismiss(); }
-  }
+	@Override
+	protected void onDestroy() {
+		destroyTts();
+		super.onDestroy();
+		if (dialog != null)
+		{
+			dialog.dismiss();
+		}
+	}
 }
