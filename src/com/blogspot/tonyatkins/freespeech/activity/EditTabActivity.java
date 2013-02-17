820953c7f6947d1a65c9c70f3bd299084d5a71de
Tony Atkins <3868423+duhrer@users.noreply.github.com>
2013-02-17 22:27:52 +0100

    Updated color handling to use integers for all colors, including in the database.

--- src/com/blogspot/tonyatkins/freespeech/activity/EditTabActivity.java
+++ src/com/blogspot/tonyatkins/freespeech/activity/EditTabActivity.java
@@ -41,6 +41,8 @@ import com.blogspot.tonyatkins.freespeech.db.DbAdapter;
 import com.blogspot.tonyatkins.freespeech.model.Tab;
 import com.blogspot.tonyatkins.freespeech.view.ColorSwatch;
 import com.blogspot.tonyatkins.freespeech.watchers.TabLabelTextUpdateWatcher;
+import com.blogspot.tonyatkins.picker.activity.FilePickerActivity;
+import com.blogspot.tonyatkins.recorder.activity.RecordSoundActivity;
 
 public class EditTabActivity extends FreeSpeechActivity {
 	public static final int ADD_TAB = 6;
@@ -81,22 +83,16 @@ public class EditTabActivity extends FreeSpeechActivity {
 		// wire up the label editing
 		EditText labelEditText = (EditText) findViewById(R.id.tabLabelEditText);
 		labelEditText.setText(tempTab.getLabel());
-		labelEditText.addTextChangedListener(new TabLabelTextUpdateWatcher(tempTab, Tab.LABEL_TEXT_TYPE));
+		labelEditText.addTextChangedListener(new TabLabelTextUpdateWatcher(tempTab));
 		
 		// wire up the background color editing
 		colorSwatch = (ColorSwatch) findViewById(R.id.tabBgColorColorSwatch);
 		colorSwatch.setBackgroundColor(Color.TRANSPARENT);
-		try {
-			if (tempTab.getBgColor() != null) {
-				colorSwatch.setBackgroundColor(Color.parseColor(tempTab.getBgColor()));
-			}
-		} catch (IllegalArgumentException e) {
-			Toast.makeText(this, "The current color is invalid and will not be displayed.", Toast.LENGTH_LONG).show();
-		}
+		colorSwatch.setBackgroundColor(tempTab.getBgColor());
 		
 		// launch a color picker activity when this view is clicked
 		Bundle pickColorBundle = new Bundle();
-		pickColorBundle.putString(ColorPickerActivity.COLOR_BUNDLE, tempTab.getBgColor());
+		pickColorBundle.putInt(ColorPickerActivity.COLOR_BUNDLE, tempTab.getBgColor());
 		colorSwatch.setOnClickListener(new LaunchIntentListener(this, ColorPickerActivity.class, pickColorBundle));
 
 		// wire up the cancel button
@@ -134,36 +130,25 @@ public class EditTabActivity extends FreeSpeechActivity {
 			}
 			else 
 			{
-				try 
-				{
-					if (tempTab.getBgColor() != null) {
-						Color.parseColor(tempTab.getBgColor());
-					}
-					Intent returnedIntent = new Intent();
-					boolean saveSuccessful;
-					if (isNewTab) {
-						Long tabId = dbAdapter.createTab(tempTab);
-						saveSuccessful = tabId != -1;
-						Bundle bundle = new Bundle();
-						bundle.putString(Tab.TAB_ID_BUNDLE, String.valueOf(tabId));
-						returnedIntent.putExtras(bundle);
-					}
-					else {
-						saveSuccessful = dbAdapter.updateTab(tempTab);
-					}
-					
-					if (saveSuccessful) {
-						setResult(RESULT_OK,returnedIntent);
-						finish();
-					}
-					else {
-						Toast.makeText(context, "There was an error saving this tab.", Toast.LENGTH_LONG).show();
-					}
-				} 
-				catch (IllegalArgumentException e) 
-				{
-					// catch an exception if we've been passed an invalid color
-					Toast.makeText(context, "You chose an invalid color, can't continue.", Toast.LENGTH_LONG).show();
+				Intent returnedIntent = new Intent();
+				boolean saveSuccessful;
+				if (isNewTab) {
+					Long tabId = dbAdapter.createTab(tempTab);
+					saveSuccessful = tabId != -1;
+					Bundle bundle = new Bundle();
+					bundle.putString(Tab.TAB_ID_BUNDLE, String.valueOf(tabId));
+					returnedIntent.putExtras(bundle);
+				}
+				else {
+					saveSuccessful = dbAdapter.updateTab(tempTab);
+				}
+				
+				if (saveSuccessful) {
+					setResult(RESULT_OK,returnedIntent);
+					finish();
+				}
+				else {
+					Toast.makeText(context, "There was an error saving this tab.", Toast.LENGTH_LONG).show();
 				}
 			}	
 		}
@@ -207,22 +192,16 @@ public class EditTabActivity extends FreeSpeechActivity {
 			Bundle returnedBundle = data.getExtras();
 			if (returnedBundle != null) {
 				if (requestCode == ColorPickerActivity.REQUEST_CODE && resultCode == ColorPickerActivity.COLOR_SELECTED) {
-						String selectedColorString = returnedBundle.getString(ColorPickerActivity.COLOR_BUNDLE);
-						try {
-							// This will throw an exception if the color isn't valid
-							int selectedColor = Color.parseColor(selectedColorString);
-							colorSwatch.setBackgroundColor(selectedColor);
-							tempTab.setBgColor(selectedColorString);
-						} catch (IllegalArgumentException e) {
-							Toast.makeText(this, "Invalid color returned from color picker, ignoring.", Toast.LENGTH_LONG).show();
-						}
+						int selectedColor = returnedBundle.getInt(ColorPickerActivity.COLOR_BUNDLE);
+						colorSwatch.setBackgroundColor(selectedColor);
+						tempTab.setBgColor(selectedColor);
 				}
 			}
 			else {
 				// If no data is returned from the color picker, but the result is OK, it means the color is set to transparent (null)
 				if (requestCode == ColorPickerActivity.REQUEST_CODE && resultCode == ColorPickerActivity.COLOR_SELECTED) {
 					colorSwatch.setBackgroundColor(Color.TRANSPARENT);
-					tempTab.setBgColor(null);
+					tempTab.setBgColor(Color.TRANSPARENT);
 				}
 			}
 		}
